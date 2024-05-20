8f8b99c0389e3e64925c9d4d6f2332b8183efd82
Shaampuu <161147293+Shaampuu@users.noreply.github.com>
2024-05-20 09:14:52 -0500

    Tipo Vehiculo y Vehiculo

--- Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/TipoVehiculo.java
+++ Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/TipoVehiculo.java
@@ -1,5 +1,5 @@
 package co.edu.uniquindio.poo;
 
 public enum TipoVehiculo {
-    CARRO, MOTO_CLASICA, MOTO_HIBRIDA;
+    CARRO, MOTO;
 }
--- Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/Vehiculo.java
+++ Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/Vehiculo.java
@@ -4,44 +4,36 @@ public abstract class Vehiculo {
     private String placa; 
     private int modelo;
     private Propietario propietario;
-    private TipoVehiculo tipoVehiculo;
 
-    public Vehiculo (String placa, int modelo, Propietario propietario, TipoVehiculo tipoVehiculo){
+    public Vehiculo(String placa, int modelo, Propietario propietario) {
         assert placa != null && !placa.isBlank() : "La placa debe ser diferente de null";
-        assert modelo > 0 : "el modelo del vehiculo debe ser mayor a 0 (cero)";
-        assert tipoVehiculo != null : "El tipo de vehiculo debe ser diferente de null";
-        
-        this.placa=placa;
-        this.modelo=modelo;
+        assert modelo > 0 : "El modelo del vehiculo debe ser mayor a 0 (cero)";
+        assert propietario != null : "El propietario debe ser diferente de null";
+
+        this.placa = placa;
+        this.modelo = modelo;
         this.propietario = propietario;
-        this.tipoVehiculo=tipoVehiculo; 
     }
 
-    public String getPlaca(){
+    public String getPlaca() {
         return placa; 
     }
 
-    public void setPlaca(String placa){
-        this.placa=placa;
+    public void setPlaca(String placa) {
+        this.placa = placa;
     } 
 
-    public int getModelo(){
+    public int getModelo() {
         return modelo;
     }
 
-    public void setModelo(int modelo){
-        this.modelo=modelo;
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
     }
 
-    public TipoVehiculo getTipoVehiculo(){
-        return tipoVehiculo;
-    }
-
-    public void setTipoVehiculo(TipoVehiculo tipoVehiculo){
-        this.tipoVehiculo=tipoVehiculo;
-    }
-
-    public Propietario getPropietario(){
+    public Propietario getPropietario() {
         return propietario;
     }
+
+    public abstract double getTarifaPorHora();
 }
