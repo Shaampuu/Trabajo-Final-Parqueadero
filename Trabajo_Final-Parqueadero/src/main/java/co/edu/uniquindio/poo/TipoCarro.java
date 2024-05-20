bfb181c541fe673b560df0b30fc816aaab2f0c00
Shaampuu <161147293+Shaampuu@users.noreply.github.com>
2024-05-20 09:28:35 -0500

    TipoCarro

--- Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/TipoCarro.java
+++ Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/TipoCarro.java
@@ -1,7 +1,7 @@
 package co.edu.uniquindio.poo;
 
 public enum TipoCarro {
-    CARRO(TipoVehiculo.CARRO), CAMIONETA(TipoVehiculo.CARRO), DEPORTIVO(TipoVehiculo.CARRO), BUS(TipoVehiculo.CARRO);
+    CARRO(TipoVehiculo.CARRO), CAMIONETA(TipoVehiculo.CARRO), DEPORTIVO(TipoVehiculo.CARRO);
 
     private final TipoVehiculo tipoVehiculo;
 
